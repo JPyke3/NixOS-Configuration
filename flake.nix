7c03c343d3144da4a05b006d0e4830785a9fc879
JPyke3 <pyke.jacob1@gmail.com>
2024-02-26 22:07:32 +1000

    NixOS Rebuild: 26 current  2024-02-26 20:17:17  23.11.20240225.5bf1cad   6.1.79                          *

--- flake.nix
+++ flake.nix
@@ -2,7 +2,12 @@
   description = "Jacobs NixOS System Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs"; # MESA/OpenGL HW workaround
+    };
   };
 
   outputs = {
