c2cbc49df04e00f3ae7e38dec50ed8a26debe004
JPyke3 <pyke.jacob1@gmail.com>
2024-02-26 20:26:40 +1000

    NixOS Rebuild: 26 current  2024-02-26 20:17:17  23.11.20240225.5bf1cad   6.1.79                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,14 +1,16 @@
 #nix-shell -p linuxPackages.nvidia_x11 Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -17,7 +19,7 @@
   networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Australia/Brisbane";
@@ -58,13 +60,13 @@
   hardware.pulseaudio.enable = true;
 
   hardware.opengl = {
-	enable = true;
-	driSupport = true;
-	driSupport32Bit = true;
-	extraPackages = with pkgs; [
-		amdvlk
-		driversi686Linux.amdvlk
-     ];
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      amdvlk
+      driversi686Linux.amdvlk
+    ];
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -76,7 +78,7 @@
   users.users.jacobpyke = {
     isNormalUser = true;
     home = "/home/jacobpyke";
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
     ];
@@ -87,20 +89,21 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-	kitty
-	wofi
-	blueman
-	amdgpu_top
-	htop
+    kitty
+    wofi
+    blueman
+    amdgpu_top
+    htop
+    alejandra
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-   programs.mtr.enable = true;
-   programs.gnupg.agent = {
-     enable = true;
-     enableSSHSupport = true;
-   };
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
@@ -135,6 +138,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
-
 }
-
