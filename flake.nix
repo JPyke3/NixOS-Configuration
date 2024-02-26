5c68763334a4913aa87d9d871eac3acc75fa3fee
JPyke3 <pyke.jacob1@gmail.com>
2024-02-26 22:24:58 +1000

    NixOS Rebuild: 27 current  2024-02-26 22:08:37  23.11.20240225.5bf1cad   6.1.79                          *

--- flake.nix
+++ flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs_unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprland/nixos";
   };
 
   outputs = {
@@ -12,6 +14,7 @@
   } @ inputs: {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
       modules = [
         ./hosts/desktop/configuration.nix
       ];
