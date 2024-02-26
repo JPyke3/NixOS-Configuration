3969e268fc0cf72532b4d33b687092293633ecc8
JPyke3 <pyke.jacob1@gmail.com>
2024-02-26 22:30:07 +1000

    NixOS Rebuild: 27 current  2024-02-26 22:08:37  23.11.20240225.5bf1cad   6.1.79                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -136,35 +136,5 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
 
-  nixpkgs.overlays = [
-    (final: prev: {
-      hyprland = inputs.hyprland.packages.${pkgs.system}.hyprland;
-      wlroots-hyprland = inputs.hyprland.packages.${pkgs.system}.wlroots-hyprland;
-      wlroots = inputs.nixpkgs_unstable.legacyPackages.${pkgs.system}.wlroots;
-    })
-    (final: prev: {
-      wlroots = prev.wlroots.override {
-        xwayland = prev.xwayland;
-        mesa = pkgs.mesa;
-      };
-    })
-    (final: prev: {
-      wlroots = prev.wlroots.overrideAttrs (old: {
-        nativeBuildInputs =
-          old.nativeBuildInputs
-          ++ [inputs.nixpkgs_unstable.legacyPackages.${pkgs.system}.libdrm];
-      });
-    })
-    (final: prev: {
-      wlroots-hyprland = prev.wlroots-hyprland.override {wlroots = prev.wlroots;};
-    })
-    (final: prev: {
-      hyprland = prev.hyprland.override {
-        mesa = pkgs.mesa;
-        wlroots = prev.wlroots-hyprland;
-      };
-    })
-  ];
-
   system.stateVersion = "23.11"; # Did you read the comment?
 }
