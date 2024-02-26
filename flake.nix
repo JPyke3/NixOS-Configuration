ae44efe2cc47f968d4177493ee51ff70a3652e2d
JPyke3 <pyke.jacob1@gmail.com>
2024-02-26 21:59:42 +1000

    NixOS Rebuild: 26 current  2024-02-26 20:17:17  23.11.20240225.5bf1cad   6.1.79                          *

--- flake.nix
+++ flake.nix
@@ -4,33 +4,34 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs.overlays = [
-    (final: prev:
-      {
+      (final: prev: {
         hyprland = inputs.hyprland.packages.${pkgs.system}.hyprland;
         wlroots-hyprland = inputs.hyprland.packages.${pkgs.system}.wlroots-hyprland;
         wlroots = inputs.nixpkgs_unstable.legacyPackages.${pkgs.system}.wlroots;
       })
-    (final: prev: {
-      wlroots = prev.wlroots.override {
-        xwayland = prev.xwayland;
-        mesa = pkgs.mesa;
-      };
-    })
-    (final: prev: {
-      wlroots = prev.wlroots.overrideAttrs (old: {
-        nativeBuildInputs = old.nativeBuildInputs ++
-          [ inputs.nixpkgs_unstable.legacyPackages.${pkgs.system}.libdrm ];
-      });
-    })
-    (final: prev: {
-      wlroots-hyprland = prev.wlroots-hyprland.override { wlroots = prev.wlroots; };
-    })
-    (final: prev: {
-      hyprland = prev.hyprland.override {
-        mesa = pkgs.mesa;
-        wlroots = prev.wlroots-hyprland;
-      };
-    })
+      (final: prev: {
+        wlroots = prev.wlroots.override {
+          xwayland = prev.xwayland;
+          mesa = pkgs.mesa;
+        };
+      })
+      (final: prev: {
+        wlroots = prev.wlroots.overrideAttrs (old: {
+          nativeBuildInputs =
+            old.nativeBuildInputs
+            ++ [inputs.nixpkgs_unstable.legacyPackages.${pkgs.system}.libdrm];
+        });
+      })
+      (final: prev: {
+        wlroots-hyprland = prev.wlroots-hyprland.override {wlroots = prev.wlroots;};
+      })
+      (final: prev: {
+        hyprland = prev.hyprland.override {
+          mesa = pkgs.mesa;
+          wlroots = prev.wlroots-hyprland;
+        };
+      })
+    ];
   };
 
   outputs = {
