9247748a79996f39e608e52ad33dcd56a8b80edd
JPyke3 <pyke.jacob1@gmail.com>
2024-02-27 11:11:13 +1000

    NixOS Rebuild: 29 current  2024-02-27 10:45:25  23.11.20240225.5bf1cad   6.1.79                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -110,9 +110,9 @@
     enable = true;
   };
 
-  nix.settings = {
-    substituters = ["https://hyprland.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
   };
 
   system.stateVersion = "23.11"; # Did you read the comment?
